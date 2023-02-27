d2dede9fb47b15c033b920f2a6c2e161558b1df1
antonvit1 <antonbntu@gmail.com>
2023-02-27 23:30:30 +0300

    fix: correct URL

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let strOfBracketsConfig = bracketsConfig.join("").replace(/,/g, "");
+  let arrayStack = [];
+  for (let element of str) {
+    let index = strOfBracketsConfig.indexOf(element);
+    if (index % 2 === 0) {
+      if (
+        element === strOfBracketsConfig[index + 1] &&
+        arrayStack[arrayStack.length - 1] !== index
+      ) {
+        arrayStack.push(index);
+      } else if (
+        element === strOfBracketsConfig[index + 1] &&
+        arrayStack[arrayStack.length - 1] === index
+      ) {
+        arrayStack.pop();
+      } else {
+        arrayStack.push(index);
+      }
+    } else {
+      if (arrayStack.pop() !== index - 1) {
+        return false;
+      }
+    }
+  }
+  return arrayStack.length === 0;
+};
